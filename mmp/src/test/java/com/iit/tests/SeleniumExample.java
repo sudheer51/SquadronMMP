45eace6776f68819e91dbe0b35e526a987c158c8
siriseleniun <sirishakotha9@gmail.com>
2020-03-13 13:53:04 -0700

    Update the Locator

--- mmp/src/test/java/com/iit/tests/SeleniumExample.java
+++ mmp/src/test/java/com/iit/tests/SeleniumExample.java
@@ -13,9 +13,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SeleniumExample {
 	WebDriver driver;
 	
-	
-	
-	 
 	@Test
 	public void validateSearchResults()
 	{
--- mmp/src/test/java/com/iit/tests/Utility.java
+++ mmp/src/test/java/com/iit/tests/Utility.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//commented by Siris
 public class Utility {
 
 	WebDriver driver;
